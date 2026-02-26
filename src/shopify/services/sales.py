f17176754e29af41cd8f5b9b3598e52c7402ae75
Hieu Nguyen <hieu1282003@gmail.com>
2026-02-26 13:52:00 +0700

    sale func

--- src/shopify/services/sales.py
+++ src/shopify/services/sales.py
@@ -13,11 +13,40 @@ class SalesService:
     def __init__(self, client: ShopifyGraphQLClient) -> None:
         self.client = client
 
-    def create_customer(self, email: str, first_name: str, last_name: str) -> Dict[str, Any]:
+    def create_customer(self, email: str, first_name: str, last_name: str, phone: str | None = None) -> Dict[str, Any]:
         """
         TODO:
         - Implement customerCreate mutation.
         """
+        
+        query = """
+            mutation ($input: CustomerInput!) {
+                customerCreate(input: $input) {
+                        userErrors {
+                        field
+                        message
+                    }
+                        customer {
+                        id
+                        email
+                        firstName
+                        lastName
+                        phone
+                    }      
+                }
+            } 
+        """
+        
+        variables = {
+            "input" : {
+                "email": email,
+                "firstName": first_name,
+                "lastName": last_name,
+                "phone": phone if phone else None
+            }
+        }
+        
+        return self.client.execute(query=query, variables=variables)
         raise NotImplementedError
 
     def create_order(self, customer_gid: str, variant_gid: str, quantity: int) -> Dict[str, Any]:
@@ -25,6 +54,31 @@ class SalesService:
         TODO:
         - Implement OrderCreate mutation.
         """
+        
+        query = """
+            mutation ($input: DraftOrderInput!) {
+                draftOrderCreate(input: $input) {
+                    draftOrder {
+                        id
+                    }
+                }
+            }
+        """
+        
+        variables = {
+            "input": {
+                "purchasingEntity": {
+                    "customerId": customer_gid
+                },
+                "lineItems": {
+                    "quantity": quantity,
+                    "variantId": variant_gid 
+                }
+            }
+        }
+        
+        return self.client.execute(query=query, variables=variables)
+        
         raise NotImplementedError
 
     def delete_order(self, draft_order_gid: str) -> Dict[str, Any]:
@@ -32,4 +86,23 @@ class SalesService:
         TODO:
         - Implement OrderDelete mutation.
         """
+        
+        query = """
+            mutation ($input: DraftOrderDeleteInput!) {
+                draftOrderDelete(input: $input) {
+                    deletedId
+                    userErrors {
+                        field
+                        message
+                    }
+                }
+            }
+        """
+        
+        variables = {
+            "input": {
+                "id": draft_order_gid
+            }
+        }
+        return self.client.execute(query=query, variables=variables)
         raise NotImplementedError
