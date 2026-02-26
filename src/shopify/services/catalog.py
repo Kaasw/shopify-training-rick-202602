c930a69c1a4064c6a71ddde7683c7be40598e315
Hieu Nguyen <hieu1282003@gmail.com>
2026-02-26 17:08:32 +0700

    Inv and location func

--- src/shopify/services/catalog.py
+++ src/shopify/services/catalog.py
@@ -1,8 +1,37 @@
 from __future__ import annotations
 from typing import Any, Dict, Optional, List
+from enum import Enum
 
 from src.shopify.client import ShopifyGraphQLClient
 
+class InventoryAdjustmentReason(Enum):
+    CORRECTION = "correction"
+    CYCLE_COUNT_AVAILABLE = "cycle_count_available"
+    DAMAGED = "damaged"
+    MOVEMENT_CREATED = "movement_created"
+    MOVEMENT_UPDATED = "movement_updated"
+    MOVEMENT_RECEIVED = "movement_received"
+    MOVEMENT_CANCELED = "movement_canceled"
+    OTHER = "other"
+    PROMOTION = "promotion"
+    QUALITY_CONTROL = "quality_control"
+    RECEIVED = "received"
+    RESERVATION_CREATED = "reservation_created"
+    RESERVATION_DELETED = "reservation_deleted"
+    RESERVATION_UPDATED = "reservation_updated"
+    RESTOCK = "restock"
+    SAFETY_STOCK = "safety_stock"
+    SHRINKAGE = "shrinkage"
+
+class Name(Enum):
+    AVAILABLE = "available"
+    ON_HAND = "on_hand"
+    COMMITTED = "committed"
+    RESERVED = "reserved"
+    DAMAGED = "damaged"
+    SAFETY_STOCK = "safety_stock"
+    QUALITY_CONTROL = "quality_control"
+    INCOMING = "incoming"
 
 class CatalogService:
     """
@@ -44,34 +73,34 @@ class CatalogService:
         return self.client.execute(query=query, variables=variables)
         raise NotImplementedError
 
-    def create_product_with_variants(self, title: str, productOptions: List[dict]) -> Dict[str, Any]:
+    def create_product_with_variants(self, title: str, productOptions: List[dict], quantity: int = 0) -> Dict[str, Any]:
         """
         TODO:
         - Implement productCreate for a product with options (Size, Color) and variants.
         - Return response JSON.
         """
         query = """
-        mutation ($input: ProductCreateInput!) {productCreate (product: $input) {
-            product {
-                id
-                title
-                options {
+        mutation ($input: ProductCreateInput!) {
+            productCreate(product: $input) {
+                product {
                     id
-                    name
-                    position
-                    optionValues {
-                        id
-                        name
-                        hasVariants
+                    title
+                    tracksInventory 
+                    variants(first: 1) {
+                        nodes {
+                            id
+                            inventoryItem {
+                                id
+                            }
                         }
-                    }
-                }
-            userErrors {
-            field
-            message
+                    } 
+                } 
+                userErrors {
+                    field
+                    message
+                } 
             }
-        }
-    }
+        } 
     """     
         variables = {
         "input": {
@@ -272,6 +301,188 @@ class CatalogService:
         variables = {
             "id": collection_gid,
             "productIds": product_gid
+        }
+        
+        return self.client.execute(query=query, variables=variables)
+    
+    # ----------------------
+    # Locations
+    # ----------------------
+    def add_location(self, address: dict[str], name: str) -> Dict[str, Any]:
+        query = """
+            mutation ($input: LocationAddInput!) {
+                locationAdd(input: $input) {
+                    location {
+                        id
+                        name
+                    }
+                    userErrors {
+                        field
+                        message
+                    }
+                }
+                
+            }
+        """     
+        variables = {
+            "input": {
+                "address": address,
+                "name": name
+            }
+        }
+        return self.client.execute(query=query, variables=variables)
+    
+    def list_location(self, first: int = 5) -> List[Dict[str, Any]]:
+        query = """
+            query ($first: Int!) {
+                locations(first: $first) {
+                    nodes {
+                        id
+                        name
+                    }
+                }
+            }
+        """
+        
+        variables = {"first": first}
+        
+        return self.client.execute(query=query, variables=variables)
+    
+    # ----------------------
+    # Inventory
+    # ----------------------
+    def activate_inventory(self, inventory_item_gid: str, location_gid: str, quantity: int) -> Dict[str, Any]:
+        query = """
+            mutation ($inventoryItemId: ID!, $locationId: ID!, $available: Int) {
+                inventoryActivate(inventoryItemId: $inventoryItemId, locationId: $locationId, available: $available) {
+                    userErrors {
+                        field
+                        message
+                    }
+                    
+                    inventoryLevel {
+                        id
+                        quantities (names: ["available"]) {
+                            name
+                            quantity
+                        }
+                        item {
+                            id
+                        }
+                        location {
+                            id
+                        }
+                    }
+                }
+            }
+        """
+    
+        variables = {
+        "inventoryItemId":  inventory_item_gid,
+        "locationId": location_gid,
+        "available": quantity
+        }
+        
+        return self.client.execute(query=query, variables=variables)
+  
+    def update_inventory_item(self, inventory_item_gid: str, update_data: dict[str, Any]) -> Dict[str, Any]:
+        query = """
+            mutation ($id: ID!, $input: InventoryItemInput!) {
+                inventoryItemUpdate(id: $id, input: $input) {
+                    userErrors {
+                        field
+                        message
+                    }
+                    
+                    inventoryItem {
+                        id
+                        tracked
+                    }
+                }
+            }
+        """
+        
+        variables = {
+            "id": inventory_item_gid,
+            "input": update_data
+        }
+        return self.client.execute(query=query, variables=variables)
+    
+    def adjust_inventory_item_quantity(self, inventory_item_gid: str, location_gid: str, 
+                                       name: Name, 
+                                       reason: InventoryAdjustmentReason, 
+                                       referenceDocumentUri: str,
+                                       quantity: int) -> Dict[str, Any]:
+        query = """
+            mutation ($input: InventoryAdjustQuantitiesInput!) {
+                inventoryAdjustQuantities(input: $input) {
+                    userErrors {
+                        field
+                        message
+                    }
+                    
+                    inventoryAdjustmentGroup {
+                        changes {
+                            name
+                            quantityAfterChange
+                        }
+                    }
+                }
+            }
+        """
+        
+        variables = {
+            "input": {
+                "name": name.value,
+                "reason": reason.value,
+                "referenceDocumentUri": referenceDocumentUri,
+                "changes": [
+                    {
+                        "delta": quantity,
+                        "inventoryItemId": inventory_item_gid,
+                        "locationId": location_gid
+                    }
+                ]
+            }
+        }
+        
+        return self.client.execute(query=query, variables=variables)
+    
+    def set_inventory_item_quantity(self, inventory_item_gid: str, location_gid: str, 
+                                       name: Name, 
+                                       reason: InventoryAdjustmentReason, 
+                                       referenceDocumentUri: str,
+                                       quantity: int) -> Dict[str, Any]:
+        query = """
+            mutation ($input: InventorySetQuantitiesInput!) {
+                inventorySetQuantities(input: $input) {
+                    userErrors {
+                        field
+                        message
+                    }
+                    
+                    inventoryAdjustmentGroup {
+                        changes {
+                            name
+                            quantityAfterChange
+                        }
+                    }
+                }
+            }
+        """
+        
+        variables = {
+            "input": {
+                "name": name.value,
+                "reason": reason.value,
+                "referenceDocumentUri": referenceDocumentUri,
+                "quantities": {
+                    "inventoryItemId": inventory_item_gid,
+                    "locationId": location_gid,
+                    "quantity": quantity,
+                    "changeFromQuantity": None
+                }             
+            }
         }
         
         return self.client.execute(query=query, variables=variables)
