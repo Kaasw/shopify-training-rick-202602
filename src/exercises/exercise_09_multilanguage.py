be3d88f8b12e792bd0395f0f0c5ee4e7d41c5852
Hieu Nguyen <hieu1282003@gmail.com>
2026-02-27 15:38:53 +0700

    ex 9

--- src/exercises/exercise_09_multilanguage.py
+++ src/exercises/exercise_09_multilanguage.py
@@ -11,17 +11,36 @@ Goal:
 
 from src.app.config import load_settings
 from src.shopify.client import ShopifyGraphQLClient
+from src.shopify.services.catalog import CatalogService
 
 
 def main() -> None:
     settings = load_settings()
-    _client = ShopifyGraphQLClient(settings)
+    client = ShopifyGraphQLClient(settings)
+    catalog = CatalogService(client)
 
     # TODO:
     # - Implement translationsRegister mutation (or equivalent)
     # - Query translatable resources
     # - Verify translations were applied
-    raise NotImplementedError
+    
+    
+    translations = [
+      {
+        "locale": "vi",
+        "key": "title",
+        "value": "Mot cai gi day",
+        "translatableContentDigest": "41c9198e970a0c8005e7706600a3fd42e9e41664a6536a6a3f44113b2f7589e5"
+      }
+    ]
+    print(catalog.manual_translate("gid://shopify/Product/8200201830444", translations))
+    shop_locale = {
+      "published": True
+    }
+    print(catalog.update_shop_locale("vi", shop_locale))
+    
+    print(catalog.get_translated_resource("gid://shopify/Product/8200201830444", "vi"))
+    
 
 
 if __name__ == "__main__":
